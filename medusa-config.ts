bf4446d8c26db59f533d463104ce0cb9f5f4c6bd
(Jon) Phuc Nguyen <5471555+jonnguyen12@users.noreply.github.com>
2025-03-15 11:46:40 -0700

    first commit

---
+++ medusa-config.ts
@@ -0,0 +1,31 @@
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
+
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
+
+module.exports = defineConfig({
+	projectConfig: {
+		databaseUrl: process.env.DATABASE_URL,
+		http: {
+			storeCors: process.env.STORE_CORS!,
+			adminCors: process.env.ADMIN_CORS!,
+			authCors: process.env.AUTH_CORS!,
+			jwtSecret: process.env.JWT_SECRET || "supersecret",
+			cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+		},
+		redisUrl: process.env.REDIS_URL,
+	},
+	modules: {
+		eventBus: {
+			resolve: "@medusajs/event-bus-redis",
+			options: {
+				redisUrl: process.env.REDIS_URL,
+			},
+		},
+		cacheService: {
+			resolve: "@medusajs/cache-redis",
+			options: {
+				redisUrl: process.env.REDIS_URL,
+			},
+		},
+	},
+});
